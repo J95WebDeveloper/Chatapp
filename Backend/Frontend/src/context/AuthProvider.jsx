c2ed939ce396634251a62a21ff2ac9ceeb1f308a
J95WebDeveloper <164620830+J95WebDeveloper@users.noreply.github.com>
2026-04-02 12:56:10 +0530

    Update AuthProvider.jsx

--- Backend/Frontend/src/context/AuthProvider.jsx
+++ Backend/Frontend/src/context/AuthProvider.jsx
@@ -1,11 +1,26 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const user = Cookies.get("jwt") || localStorage.getItem("chat-user");
-  const [authUser, setAuthUser] = useState(user ? JSON.parse(user) : null);
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get("jwt");
+    const user = localStorage.getItem("chat-user");
+
+    if (token && user) {
+      try {
+        setAuthUser(JSON.parse(user));
+      } catch (err) {
+        localStorage.removeItem("chat-user");
+        setAuthUser(null);
+      }
+    } else {
+      setAuthUser(null);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ authUser, setAuthUser }}>
