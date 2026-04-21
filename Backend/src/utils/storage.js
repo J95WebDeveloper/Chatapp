82e1f545e9f30d519474ff8bd2ca44c6f5725f58
J95WebDeveloper <164620830+J95WebDeveloper@users.noreply.github.com>
2026-04-21 19:44:55 +0530

    Update storage.js

--- Backend/src/utils/storage.js
+++ Backend/src/utils/storage.js
@@ -1,12 +1,21 @@
-import multer from "multer";
-import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-export const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+export const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => ({
+    folder: "profile-images",
+    allowed_formats: ["jpg", "png", "jpeg"]
+  }),
+});
+
