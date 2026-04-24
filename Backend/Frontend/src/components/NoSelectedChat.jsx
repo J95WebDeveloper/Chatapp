db97e73b8dca0e924eb84d6995a51cba6c76c928
J95WebDeveloper <164620830+J95WebDeveloper@users.noreply.github.com>
2026-04-24 13:26:45 +0530

    Update NoSelectedChat.jsx

--- Backend/Frontend/src/components/NoSelectedChat.jsx
+++ Backend/Frontend/src/components/NoSelectedChat.jsx
@@ -8,8 +8,8 @@ function NoSelectedChat() {
   const {authUser} = useAuth()
   return (
     <div className='bg flex justify-center items-center h-full'>
-        <div className='grid gap-5'>
-            <h1 className='flex text-2xl md:text-5xl lg:text-6xl font-bold items-center'> Welcome <img src={Chatbot} className='w-8 md:w-16 mx-4 md:mx-6' /> <span> {authUser.fullname} </span></h1>
+        <div className='grid gap-3 lg:gap-5'>
+            <h1 className='flex text-3xl md:text-5xl lg:text-6xl font-bold items-center'> Welcome <img src={Chatbot} className='w-8 md:w-16 mx-4 md:mx-6' /> <span> {authUser.fullname} </span></h1>
             <p className='text-center text-md md:text-2xl font-medium'> Let's start chat</p>
         </div>
     </div>
